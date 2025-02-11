e3e71a0a2a3dbd19c87edfac7bf7b3329af9dd17
Myllena Medeiros <myllenamed19@gmail.com>
2025-02-11 18:02:57 -0300

    Acrescentando comentários aos posts

--- src/main/java/com/myllamedeiros/workshopmongo/config/Instantiation.java
+++ src/main/java/com/myllamedeiros/workshopmongo/config/Instantiation.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import com.myllamedeiros.workshopmongo.domain.Post;
 import com.myllamedeiros.workshopmongo.domain.User;
 import com.myllamedeiros.workshopmongo.dto.AuthorDTO;
+import com.myllamedeiros.workshopmongo.dto.CommentDTO;
 import com.myllamedeiros.workshopmongo.repository.PostRepository;
 import com.myllamedeiros.workshopmongo.repository.UserRepository;
 
@@ -40,6 +41,13 @@ public class Instantiation implements CommandLineRunner {
 		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Nave da República", "Aprendendo a pilotar. Vlw Orion!", new AuthorDTO(pax));
 		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Aprendendo com a Soberana", "Vou ter que assumir isso um dia então é melhor aprender :|", new AuthorDTO(pax));
 
+		CommentDTO c1 = new CommentDTO("Vai destruir a nave haha!", sdf.parse("21/03/2018"), new AuthorDTO(electra));
+		CommentDTO c2 = new CommentDTO("Você tem que me ensinar depois, não sei muito bem ainda :/", sdf.parse("22/03/2018"), new AuthorDTO(lyria));
+		CommentDTO c3 = new CommentDTO("Que o Reaper nos ajude!!", sdf.parse("23/03/2018"), new AuthorDTO(electra));
+
+		post1.getComments().addAll(Arrays.asList(c1,c2));
+		post2.getComments().addAll(Arrays.asList(c3));
+		
 		postRep.saveAll(Arrays.asList(post1,post2));
 	
 		pax.getPosts().add(post1);
