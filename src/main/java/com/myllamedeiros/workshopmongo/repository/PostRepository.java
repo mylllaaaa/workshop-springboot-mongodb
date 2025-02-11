576681d2f72c986faee824d21191838e2dd63921
Myllena Medeiros <myllenamed19@gmail.com>
2025-02-11 18:42:46 -0300

    Consulta simples com @Query

--- src/main/java/com/myllamedeiros/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/myllamedeiros/workshopmongo/repository/PostRepository.java
@@ -3,6 +3,7 @@ package com.myllamedeiros.workshopmongo.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.myllamedeiros.workshopmongo.domain.Post;
@@ -10,6 +11,9 @@ import com.myllamedeiros.workshopmongo.domain.Post;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
 
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }\r\n")
+	List<Post> searchTitle(String text);
+	
 	List<Post> findByTitleContainingIgnoreCase(String text);
 }
 
--- src/main/java/com/myllamedeiros/workshopmongo/services/PostService.java
+++ src/main/java/com/myllamedeiros/workshopmongo/services/PostService.java
@@ -21,7 +21,7 @@ public class PostService {
 	}	
 	
 	public List<Post> findByTitle(String text){
-		return rep.findByTitleContainingIgnoreCase(text);
+		return rep.searchTitle(text);
 	}
 	
 }
